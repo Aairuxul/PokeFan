854853fc1f79e2230ae9e6ae66d48e9bab3ba63a
Tommy <tommy.angibaud@gmail.com>
2026-05-22 09:27:55 +0200

    feat: Add top navigation menu with dynamic profile picture

---
+++ src/app/(components)/navmenue.tsx
@@ -0,0 +1,17 @@
+async function NavMenue() {
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+    let profilePicture = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`).then(res => res.json());
+  return (
+    <nav className="w-full h-16 bg-gray-800 text-white flex items-center justify-between px-4">
+      <div className="text-lg font-bold"></div>
+      <ul className="flex space-x-4">
+        <li><a href="/" className="hover:text-gray-400">Compta</a></li>
+      </ul>
+      <div>
+        <img src={profilePicture.sprites.front_default} alt="Profile" className="w-8 h-8 rounded-full" />
+      </div>
+    </nav>
+  )
+}
+
+export default NavMenue;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavMenue from "./(components)/navmenue";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -66,7 +67,7 @@ const menuLayout = ({ children }: { children: React.ReactNode }) => {
           </ul>
         </nav>
       </aside>
-      <main className="flex-1 p-6 bg-[var(--background)] text-[var(--foreground)]">{children}</main>
+      <main className="flex-1 p-6 bg-[var(--background)] text-[var(--foreground)]"><NavMenue />{children}</main>
     </div>
   );
 };
